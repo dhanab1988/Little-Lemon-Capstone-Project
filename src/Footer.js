277d0f804e3b8f8ff4dd360e4c2363e5dbad53ec
dhanab1988 <dhanab1988@gmail.com>
2025-12-02 18:13:32 +0530

    design header

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,10 @@
-function Footer()
-{
-   return
-    (
-    <footer>
-        <p>© 2025 Little Lemon Restaurant</p>
-        <p>Contact: info@littlelemon.com</p>
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Little Lemon Restaurant</p>
+      <p>Contact: info@littlelemon.com</p>
     </footer>
-    )
+  );
 }
+
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,32 @@
-
-function Header()
-{
-
-
-    return(
-        
-    <header>
-        <img src="/Logo.svg" alt="Little Lemon Logo" />
-
-        </header>
-    )
+/* function Header() {
+  return (
+    <header className="header">
+      <img id="logo" className="logo" src="/Logo.svg" alt="Little Lemon Logo" />
+    </header>
+  );
 }
+
+export default Header; */
+
+function Header() {
+  return (
+   
+    <header className="header">
+    <img className="logo" src="/Logo.svg" alt="Little Lemon Logo" />
+
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Reservations</a></li>
+        <li><a href="#">Order Online</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </nav>
+</header>
+
+  );
+}
+
 export default Header;
